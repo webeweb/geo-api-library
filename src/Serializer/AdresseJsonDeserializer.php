1d7e8bf4756e6b26828c736ad99b462c8e08a2b6
webeweb <support@webeweb.fr>
2020-05-28 19:38:59 +0200

    Add Adresse deserializer

---
+++ src/Serializer/AdresseJsonDeserializer.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Serializer;
+
+use WBW\Library\GeoJSON\Serializer\JsonDeserializer as BaseJsonDeserializer;
+
+/**
+ * Adresse JSON deserializer.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Serializer
+ */
+class AdresseJsonDeserializer extends BaseJsonDeserializer {
+
+}
