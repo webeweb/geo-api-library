2549c088f3a1ce6f2c2da89fc37666a8894b056d
webeweb <support@webeweb.fr>
2020-05-29 20:34:28 +0200

    Add Adresse model

---
+++ src/Model/Adresse.php
@@ -0,0 +1,414 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Model;
+
+use WBW\Library\Core\Model\Attribute\StringLabelTrait;
+use WBW\Library\Core\Model\Attribute\StringNameTrait;
+use WBW\Library\Core\Model\Attribute\StringTypeTrait;
+
+/**
+ * Adresse.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Model
+ */
+class Adresse {
+
+    use StringLabelTrait;
+    use StringNameTrait;
+    use StringTypeTrait;
+
+    /**
+     * City.
+     *
+     * @var string
+     */
+    private $city;
+
+    /**
+     * City code.
+     *
+     * @var string
+     */
+    private $cityCode;
+
+    /**
+     * Context.
+     *
+     * @var string
+     */
+    private $context;
+
+    /**
+     * Distance.
+     *
+     * @var string
+     */
+    private $distance;
+
+    /**
+     * District.
+     *
+     * @var string
+     */
+    private $district;
+
+    /**
+     * House number.
+     *
+     * @var string
+     */
+    private $houseNumber;
+
+    /**
+     * Id.
+     *
+     * @var string.
+     */
+    private $id;
+
+    /**
+     * Latitude.
+     *
+     * @var string
+     */
+    private $latitude;
+
+    /**
+     * Longitude.
+     *
+     * @var string
+     */
+    private $longitude;
+
+    /**
+     * Old city.
+     *
+     * @var string
+     */
+    private $oldCity;
+
+    /**
+     * Old city code.
+     *
+     * @var string
+     */
+    private $oldCityCode;
+
+    /**
+     * Postcode.
+     *
+     * @var string
+     */
+    private $postcode;
+
+    /**
+     * Score.
+     *
+     * @var string
+     */
+    private $score;
+
+    /**
+     * Street.
+     *
+     * @var string
+     */
+    private $street;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO.
+    }
+
+    /**
+     * Get the city.
+     *
+     * @return string Returns the city.
+     */
+    public function getCity() {
+        return $this->city;
+    }
+
+    /**
+     * Get the city code.
+     *
+     * @return string Returns the city.
+     */
+    public function getCityCode() {
+        return $this->cityCode;
+    }
+
+    /**
+     * Get the context.
+     *
+     * @return string Returns the context.
+     */
+    public function getContext() {
+        return $this->context;
+    }
+
+    /**
+     * Get the distance.
+     *
+     * @return string Returns the distance.
+     */
+    public function getDistance() {
+        return $this->distance;
+    }
+
+    /**
+     * Get the district.
+     *
+     * @return string Returns the district.
+     */
+    public function getDistrict() {
+        return $this->district;
+    }
+
+    /**
+     * Get the house number.
+     *
+     * @return string Returns the house number.
+     */
+    public function getHouseNumber() {
+        return $this->houseNumber;
+    }
+
+    /**
+     * Get the id.
+     *
+     * @return string Returns the id.
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set the latitude.
+     *
+     * @return string Returns the latitude.
+     */
+    public function getLatitude() {
+        return $this->latitude;
+    }
+
+    /**
+     * Get the longitude.
+     *
+     * @return string Returns the longitude.
+     */
+    public function getLongitude() {
+        return $this->longitude;
+    }
+
+    /**
+     * Get the old city.
+     *
+     * @return string Returns the old city.
+     */
+    public function getOldCity() {
+        return $this->oldCity;
+    }
+
+    /**
+     * Get the old city code.
+     *
+     * @return string Returns the old city code.
+     */
+    public function getOldCityCode() {
+        return $this->oldCityCode;
+    }
+
+    /**
+     * Get the postcode.
+     *
+     * @return string Returns the postcode.
+     */
+    public function getPostcode() {
+        return $this->postcode;
+    }
+
+    /**
+     * Get the score.
+     *
+     * @return string Returns the score.
+     */
+    public function getScore() {
+        return $this->score;
+    }
+
+    /**
+     * Get the street.
+     *
+     * @return string Returns the street.
+     */
+    public function getStreet() {
+        return $this->street;
+    }
+
+    /**
+     * Set the city.
+     *
+     * @param string $city The city.
+     * @return Adresse Returns this adresse.
+     */
+    public function setCity($city) {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * Set the city code.
+     *
+     * @param string $cityCode The city code.
+     * @return Adresse Returns this adresse.
+     */
+    public function setCityCode($cityCode) {
+        $this->cityCode = $cityCode;
+        return $this;
+    }
+
+    /**
+     * Set the context.
+     *
+     * @param string $context The context.
+     * @return Adresse Returns this adresse.
+     */
+    public function setContext($context) {
+        $this->context = $context;
+        return $this;
+    }
+
+    /**
+     * The distance.
+     *
+     * @param string $distance The distance.
+     * @return Adresse Returns this adresse.
+     */
+    public function setDistance($distance) {
+        $this->distance = $distance;
+        return $this;
+    }
+
+    /**
+     * Set the district.
+     *
+     * @param string $district The district.
+     * @return Adresse Returns this adresse.
+     */
+    public function setDistrict($district) {
+        $this->district = $district;
+        return $this;
+    }
+
+    /**
+     * Set the house number.
+     *
+     * @param string $houseNumber The house number.
+     * @return Adresse Returns this adresse.
+     */
+    public function setHouseNumber($houseNumber) {
+        $this->houseNumber = $houseNumber;
+        return $this;
+    }
+
+    /**
+     * Set the id.
+     *
+     * @param string $id The id.
+     * @return Adresse Returns this adresse.
+     */
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Set the latitude.
+     *
+     * @param string $latitude The latitude.
+     * @return Adresse Returns this adresse.
+     */
+    public function setLatitude($latitude) {
+        $this->latitude = $latitude;
+        return $this;
+    }
+
+    /**
+     * Set the longitude.
+     *
+     * @param string $longitude The longitude.
+     * @return Adresse Returns this adresse.
+     */
+    public function setLongitude($longitude) {
+        $this->longitude = $longitude;
+        return $this;
+    }
+
+    /**
+     * Set the old city.
+     *
+     * @param string $oldCity The old city.
+     * @return Adresse Returns this adresse.
+     */
+    public function setOldCity($oldCity) {
+        $this->oldCity = $oldCity;
+        return $this;
+    }
+
+    /**
+     * Set the old city code.
+     *
+     * @param string $oldCityCode The old city code.
+     * @return Adresse Returns this adresse.
+     */
+    public function setOldCityCode($oldCityCode) {
+        $this->oldCityCode = $oldCityCode;
+        return $this;
+    }
+
+    /**
+     * Set the postcode.
+     *
+     * @param string $postcode The postcode.
+     * @return Adresse Returns this adresse.
+     */
+    public function setPostcode($postcode) {
+        $this->postcode = $postcode;
+        return $this;
+    }
+
+    /**
+     * Set the score.
+     *
+     * @param string $score Teh score.
+     * @return Adresse Returns this adresse.
+     */
+    public function setScore($score) {
+        $this->score = $score;
+        return $this;
+    }
+
+    /**
+     * Set the street.
+     *
+     * @param string $street Teh street.
+     * @return Adresse Returns this adresse.
+     */
+    public function setStreet($street) {
+        $this->street = $street;
+        return $this;
+    }
+}
---
+++ tests/Model/AdresseTest.php
@@ -0,0 +1,234 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Tests\Model;
+
+use WBW\Library\GeoAPI\Model\Adresse;
+use WBW\Library\GeoAPI\Tests\AbstractTestCase;
+
+/**
+ * Adresse test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Tests\Model
+ */
+class AdresseTest extends AbstractTestCase {
+
+    /**
+     * Tests the setCity() method.
+     *
+     * @return void
+     */
+    public function testSetCity() {
+
+        $obj = new Adresse();
+
+        $obj->setCity("city");
+        $this->assertEquals("city", $obj->getCity());
+    }
+
+    /**
+     * Tests the setCityCode() method.
+     *
+     * @return void
+     */
+    public function testSetCityCode() {
+
+        $obj = new Adresse();
+
+        $obj->setCityCode("cityCode");
+        $this->assertEquals("cityCode", $obj->getCityCode());
+    }
+
+    /**
+     * Tests the setContext() method.
+     *
+     * @return void
+     */
+    public function testSetContext() {
+
+        $obj = new Adresse();
+
+        $obj->setContext("context");
+        $this->assertEquals("context", $obj->getContext());
+    }
+
+    /**
+     * Tests the setDistance() method.
+     *
+     * @return void
+     */
+    public function testSetDistance() {
+
+        $obj = new Adresse();
+
+        $obj->setDistance("distance");
+        $this->assertEquals("distance", $obj->getDistance());
+    }
+
+    /**
+     * Tests the setDistrict() method.
+     *
+     * @return void
+     */
+    public function testSetDistrict() {
+
+        $obj = new Adresse();
+
+        $obj->setDistrict("district");
+        $this->assertEquals("district", $obj->getDistrict());
+    }
+
+    /**
+     * Tests the setHouseNumber() method.
+     *
+     * @return void
+     */
+    public function testSetHouseNumber() {
+
+        $obj = new Adresse();
+
+        $obj->setHouseNumber("houseNumber");
+        $this->assertEquals("houseNumber", $obj->getHouseNumber());
+    }
+
+    /**
+     * Tests the setId() method.
+     *
+     * @return void
+     */
+    public function testSetId() {
+
+        $obj = new Adresse();
+
+        $obj->setId("id");
+        $this->assertEquals("id", $obj->getId());
+    }
+
+    /**
+     * Tests the setLatitude() method.
+     *
+     * @return void
+     */
+    public function testSetLatitude() {
+
+        $obj = new Adresse();
+
+        $obj->setLatitude("latitude");
+        $this->assertEquals("latitude", $obj->getLatitude());
+    }
+
+    /**
+     * Tests the setLongitude() method.
+     *
+     * @return void
+     */
+    public function testSetLongitude() {
+
+        $obj = new Adresse();
+
+        $obj->setLongitude("longitude");
+        $this->assertEquals("longitude", $obj->getLongitude());
+    }
+
+    /**
+     * Tests the setOldCity() method.
+     *
+     * @return void
+     */
+    public function testSetOldCity() {
+
+        $obj = new Adresse();
+
+        $obj->setOldCity("oldCity");
+        $this->assertEquals("oldCity", $obj->getOldCity());
+    }
+
+    /**
+     * Tests the setOldCityCode() method.
+     *
+     * @return void
+     */
+    public function testSetOldCityCode() {
+
+        $obj = new Adresse();
+
+        $obj->setOldCityCode("oldCityCode");
+        $this->assertEquals("oldCityCode", $obj->getOldCityCode());
+    }
+
+    /**
+     * Tests the setPostcode() method.
+     *
+     * @return void
+     */
+    public function testSetPostcode() {
+
+        $obj = new Adresse();
+
+        $obj->setPostcode("postcode");
+        $this->assertEquals("postcode", $obj->getPostcode());
+    }
+
+    /**
+     * Tests the setScore() method.
+     *
+     * @return void
+     */
+    public function testSetScore() {
+
+        $obj = new Adresse();
+
+        $obj->setScore("score");
+        $this->assertEquals("score", $obj->getScore());
+    }
+
+    /**
+     * Tests the setStreet() method.
+     *
+     * @return void
+     */
+    public function testSetStreet() {
+
+        $obj = new Adresse();
+
+        $obj->setStreet("street");
+        $this->assertEquals("street", $obj->getStreet());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct() {
+
+        $obj = new Adresse();
+
+        $this->assertNull($obj->getCity());
+        $this->assertNull($obj->getCityCode());
+        $this->assertNull($obj->getContext());
+        $this->assertNull($obj->getDistance());
+        $this->assertNull($obj->getDistrict());
+        $this->assertNull($obj->getHouseNumber());
+        $this->assertNull($obj->getId());
+        $this->assertNull($obj->getLabel());
+        $this->assertNull($obj->getLatitude());
+        $this->assertNull($obj->getLongitude());
+        $this->assertNull($obj->getName());
+        $this->assertNull($obj->getOldCity());
+        $this->assertNull($obj->getOldCityCode());
+        $this->assertNull($obj->getPostcode());
+        $this->assertNull($obj->getScore());
+        $this->assertNull($obj->getStreet());
+        $this->assertNull($obj->getType());
+    }
+}
