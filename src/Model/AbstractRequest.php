e36cf9b188b52b71196613c969add0028e5463b6
webeweb <support@webeweb.fr>
2020-05-29 12:52:03 +0200

    Add abstract Request

---
+++ src/Model/AbstractRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Model;
+
+/**
+ * Abstract request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Model
+ * @abstract
+ */
+abstract class AbstractRequest {
+
+    /**
+     * Get the resource path.
+     *
+     * @return string Returns the resource path.
+     */
+    abstract function getResourcePath();
+}
