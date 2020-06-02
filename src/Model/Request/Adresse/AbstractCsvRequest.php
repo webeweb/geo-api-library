ad08456e286fa8f38ea67282c0f23dfcfc926260
webeweb <support@webeweb.fr>
2020-06-02 19:26:15 +0200

    Add abstract CSV request model

---
+++ src/Model/Request/Adresse/AbstractCsvRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Model\Request\Adresse;
+
+use WBW\Library\GeoAPI\Model\AbstractRequest;
+use WBW\Library\GeoAPI\Model\Attribute\StringDataTrait;
+
+/**
+ * Abstract CSV request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Model\Request\Adresse
+ * @abstract
+ */
+abstract class AbstractCsvRequest extends AbstractRequest {
+
+    use StringDataTrait;
+
+    /**
+     * Constructor.
+     *
+     * @param string $data The data.
+     */
+    public function __construct($data = null) {
+        $this->setData($data);
+    }
+}
