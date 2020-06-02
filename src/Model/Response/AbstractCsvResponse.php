044646d880969884c777cc9dbd3cd1663b38d171
webeweb <support@webeweb.fr>
2020-06-02 19:29:29 +0200

    Update abstract CSV response model: - add constants

--- src/Model/Response/AbstractCsvResponse.php
+++ src/Model/Response/AbstractCsvResponse.php
@@ -24,6 +24,125 @@ use WBW\Library\GeoAPI\Model\Adresse;
 abstract class AbstractCsvResponse extends AbstractResponse {
 
     /**
+     * Result "city".
+     *
+     * @var string
+     */
+    const RESULT_CITY = "result_city";
+
+    /**
+     * Result "city code".
+     *
+     * @var string
+     */
+    const RESULT_CITYCODE = "result_citycode";
+
+    /**
+     * Result "context".
+     *
+     * @var string
+     */
+    const RESULT_CONTEXT = "result_context";
+
+    /**
+     * Result "distance".
+     *
+     * @var string
+     */
+    const RESULT_DISTANCE = "result_distance";
+
+    /**
+     * Result "district".
+     *
+     * @var string
+     */
+    const RESULT_DISTRICT = "result_district";
+
+    /**
+     * Result "house number".
+     *
+     * @var string
+     */
+    const RESULT_HOUSENUMBER = "result_housenumber";
+
+    /**
+     * Result "id".
+     *
+     * @var string
+     */
+    const RESULT_ID = "result_id";
+
+    /**
+     * Result "label".
+     *
+     * @var string
+     */
+    const RESULT_LABEL = "result_label";
+
+    /**
+     * Result "latitude".
+     *
+     * @var string
+     */
+    const RESULT_LATITUDE = "result_latitude";
+
+    /**
+     * Result "longitude".
+     *
+     * @var string
+     */
+    const RESULT_LONGITUDE = "result_longitude";
+
+    /**
+     * Result "name".
+     *
+     * @var string
+     */
+    const RESULT_NAME = "result_name";
+
+    /**
+     * Result "old city".
+     *
+     * @var string
+     */
+    const RESULT_OLDCITY = "result_oldcity";
+
+    /**
+     * Result "old city code".
+     *
+     * @var string
+     */
+    const RESULT_OLDCITYCODE = "result_oldcitycode";
+
+    /**
+     * Result "postcode".
+     *
+     * @var string
+     */
+    const RESULT_POSTCODE = "result_postcode";
+
+    /**
+     * Result "score".
+     *
+     * @var string
+     */
+    const RESULT_SCORE = "result_score";
+
+    /**
+     * Result "street".
+     *
+     * @var string
+     */
+    const RESULT_STREET = "result_street";
+
+    /**
+     * Result "type".
+     *
+     * @var string
+     */
+    const RESULT_TYPE = "result_type";
+
+    /**
      * Adresses.
      *
      * @var Adresse[]
@@ -46,6 +165,33 @@ abstract class AbstractCsvResponse extends AbstractResponse {
     public function addAdresse(Adresse $adresse) {
         $this->adresses[] = $adresse;
         return $this;
+    }
+
+    /**
+     * Enumerates the result columns.
+     *
+     * @return string[] Returns the result columns.
+     */
+    public static function enumResultColumns() {
+        return [
+            self::RESULT_CITY,
+            self::RESULT_CITYCODE,
+            self::RESULT_CONTEXT,
+            self::RESULT_DISTANCE,
+            self::RESULT_DISTRICT,
+            self::RESULT_HOUSENUMBER,
+            self::RESULT_ID,
+            self::RESULT_LABEL,
+            self::RESULT_LATITUDE,
+            self::RESULT_LONGITUDE,
+            self::RESULT_NAME,
+            self::RESULT_OLDCITY,
+            self::RESULT_OLDCITYCODE,
+            self::RESULT_POSTCODE,
+            self::RESULT_SCORE,
+            self::RESULT_STREET,
+            self::RESULT_TYPE,
+        ];
     }
 
     /**
--- tests/Model/Response/AbstractCsvResponseTest.php
+++ tests/Model/Response/AbstractCsvResponseTest.php
@@ -12,6 +12,7 @@
 namespace WBW\Library\GeoAPI\Tests\Model\Response;
 
 use WBW\Library\GeoAPI\Model\Adresse;
+use WBW\Library\GeoAPI\Model\Response\AbstractCsvResponse;
 use WBW\Library\GeoAPI\Tests\AbstractTestCase;
 use WBW\Library\GeoAPI\Tests\Fixtures\Model\Response\TestCsvResponse;
 
@@ -40,11 +41,58 @@ class AbstractCsvResponseTest extends AbstractTestCase {
     }
 
     /**
+     * Tests the enumResultColumns() method.
+     *
+     * @retun void
+     */
+    public function testEnumResultColumns() {
+
+        $res = [
+            AbstractCsvResponse::RESULT_CITY,
+            AbstractCsvResponse::RESULT_CITYCODE,
+            AbstractCsvResponse::RESULT_CONTEXT,
+            AbstractCsvResponse::RESULT_DISTANCE,
+            AbstractCsvResponse::RESULT_DISTRICT,
+            AbstractCsvResponse::RESULT_HOUSENUMBER,
+            AbstractCsvResponse::RESULT_ID,
+            AbstractCsvResponse::RESULT_LABEL,
+            AbstractCsvResponse::RESULT_LATITUDE,
+            AbstractCsvResponse::RESULT_LONGITUDE,
+            AbstractCsvResponse::RESULT_NAME,
+            AbstractCsvResponse::RESULT_OLDCITY,
+            AbstractCsvResponse::RESULT_OLDCITYCODE,
+            AbstractCsvResponse::RESULT_POSTCODE,
+            AbstractCsvResponse::RESULT_SCORE,
+            AbstractCsvResponse::RESULT_STREET,
+            AbstractCsvResponse::RESULT_TYPE,
+        ];
+        $this->assertEquals($res, AbstractCsvResponse::enumResultColumns());
+    }
+
+    /**
      * Tests the __construct() method.
      *
      * @return void
      */
     public function test__construct() {
+
+        $this->assertEquals("result_city", AbstractCsvResponse::RESULT_CITY);
+        $this->assertEquals("result_citycode", AbstractCsvResponse::RESULT_CITYCODE);
+        $this->assertEquals("result_context", AbstractCsvResponse::RESULT_CONTEXT);
+        $this->assertEquals("result_distance", AbstractCsvResponse::RESULT_DISTANCE);
+        $this->assertEquals("result_district", AbstractCsvResponse::RESULT_DISTRICT);
+        $this->assertEquals("result_housenumber", AbstractCsvResponse::RESULT_HOUSENUMBER);
+        $this->assertEquals("result_id", AbstractCsvResponse::RESULT_ID);
+        $this->assertEquals("result_label", AbstractCsvResponse::RESULT_LABEL);
+        $this->assertEquals("result_latitude", AbstractCsvResponse::RESULT_LATITUDE);
+        $this->assertEquals("result_longitude", AbstractCsvResponse::RESULT_LONGITUDE);
+        $this->assertEquals("result_name", AbstractCsvResponse::RESULT_NAME);
+        $this->assertEquals("result_oldcity", AbstractCsvResponse::RESULT_OLDCITY);
+        $this->assertEquals("result_oldcitycode", AbstractCsvResponse::RESULT_OLDCITYCODE);
+        $this->assertEquals("result_postcode", AbstractCsvResponse::RESULT_POSTCODE);
+        $this->assertEquals("result_score", AbstractCsvResponse::RESULT_SCORE);
+        $this->assertEquals("result_street", AbstractCsvResponse::RESULT_STREET);
+        $this->assertEquals("result_type", AbstractCsvResponse::RESULT_TYPE);
 
         $obj = new TestCsvResponse();
 
