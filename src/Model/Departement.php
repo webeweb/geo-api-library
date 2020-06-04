494125fc18670230dd548047b91b4aefc358c46c
webeweb <support@webeweb.fr>
2020-06-04 12:49:41 +0200

    Add Departement model

---
+++ src/Model/Departement.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Model;
+
+use WBW\Library\GeoAPI\Model\Attribute\FloatScoreTrait;
+use WBW\Library\GeoAPI\Model\Attribute\StringCodeRegionTrait;
+use WBW\Library\GeoAPI\Model\Attribute\StringCodeTrait;
+use WBW\Library\GeoAPI\Model\Attribute\StringNomTrait;
+
+/**
+ * Département.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Model
+ */
+class Departement {
+
+    use FloatScoreTrait;
+    use StringCodeRegionTrait;
+    use StringCodeTrait;
+    use StringNomTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO TO.
+    }
+}
---
+++ tests/Model/DepartementTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Tests\Model;
+
+use WBW\Library\GeoAPI\Model\Departement;
+use WBW\Library\GeoAPI\Tests\AbstractTestCase;
+
+/**
+ * Département test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Tests\Model
+ */
+class DepartementTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct() {
+
+        $obj = new Departement();
+
+        $this->assertNull($obj->getCode());
+        $this->assertNull($obj->getCodeRegion());
+        $this->assertNull($obj->getNom());
+        $this->assertNull($obj->getScore());
+    }
+}
