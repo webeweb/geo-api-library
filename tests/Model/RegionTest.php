ed67130ba52c17cf7092d4789303fc2c083f81c6
webeweb <support@webeweb.fr>
2020-06-04 12:49:54 +0200

    Add Region model

---
+++ tests/Model/RegionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Tests\Model;
+
+use WBW\Library\GeoAPI\Model\Region;
+use WBW\Library\GeoAPI\Tests\AbstractTestCase;
+
+/**
+ * Région test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Tests\Model
+ */
+class RegionTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct() {
+
+        $obj = new Region();
+
+        $this->assertNull($obj->getCode());
+        $this->assertNull($obj->getNom());
+        $this->assertNull($obj->getScore());
+    }
+}
