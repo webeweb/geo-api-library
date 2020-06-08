4c0d85fc44259173bd09c89293d8fc416f3d6901
webeweb <support@webeweb.fr>
2020-06-08 13:07:06 +0200

    Rename Region response deserializer

---
+++ src/Serializer/DecoupageAdministratifResponseDeserializer.php
@@ -0,0 +1,148 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Serializer;
+
+use WBW\Library\Core\Argument\Helper\ArrayHelper;
+use WBW\Library\GeoAPI\Model\Commune;
+use WBW\Library\GeoAPI\Model\Departement;
+use WBW\Library\GeoAPI\Model\Region;
+use WBW\Library\GeoAPI\Model\Response\CommunesResponse;
+use WBW\Library\GeoAPI\Model\Response\DepartementsResponse;
+use WBW\Library\GeoAPI\Model\Response\RegionsResponse;
+
+/**
+ * Découpage administratif response deserializer.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Serializer
+ */
+class DecoupageAdministratifResponseDeserializer {
+
+    /**
+     * Deserializes a commune.
+     *
+     * @param array $response The response.
+     * @return Commune Returns the commune.
+     */
+    public static function deserializeCommune(array $response) {
+
+        $model = new Commune();
+        $model->setNom(ArrayHelper::get($response, "nom"));
+        $model->setCode(ArrayHelper::get($response, "code"));
+        $model->setCodeDepartement(ArrayHelper::get($response, "codeDepartement"));
+        $model->setCodeRegion(ArrayHelper::get($response, "codeRegion"));
+        $model->setPopulation(ArrayHelper::get($response, "population"));
+
+        return $model;
+    }
+
+    /**
+     * Deserializes a communes response.
+     *
+     * @param string $rawResponse The raw response.
+     * @return CommunesResponse Returns the communes response.
+     */
+    public static function deserializeCommunesResponse($rawResponse) {
+
+        $model = new CommunesResponse();
+        $model->setRawResponse($rawResponse);
+
+        $response = json_decode($rawResponse, true);
+        if (null === $response) {
+            return $model;
+        }
+
+        foreach ($response as $current) {
+            $model->addCommune(static::deserializeCommune($current));
+        }
+
+        return $model;
+    }
+
+    /**
+     * Deserializes a département.
+     *
+     * @param array $response The response.
+     * @return Departement Returns the département.
+     */
+    public static function deserializeDepartement(array $response) {
+
+        $model = new Departement();
+        $model->setNom(ArrayHelper::get($response, "nom"));
+        $model->setCode(ArrayHelper::get($response, "code"));
+        $model->setCodeRegion(ArrayHelper::get($response, "codeRegion"));
+
+        return $model;
+    }
+
+    /**
+     * Deserializes a départements response.
+     *
+     * @param string $rawResponse The raw response.
+     * @return DepartementsResponse Returns the départements response.
+     */
+    public static function deserializeDepartementsResponse($rawResponse) {
+
+        $model = new DepartementsResponse();
+        $model->setRawResponse($rawResponse);
+
+        $response = json_decode($rawResponse, true);
+        if (null === $response) {
+            return $model;
+        }
+
+        foreach ($response as $current) {
+            $model->addDepartement(static::deserializeDepartement($current));
+        }
+
+        return $model;
+    }
+
+    /**
+     * Deserializes a région.
+     *
+     * @param array $response The response.
+     * @return Region Returns the région.
+     */
+    public static function deserializeRegion(array $response) {
+
+        $model = new Region();
+        $model->setCode(ArrayHelper::get($response, "code"));
+        $model->setNom(ArrayHelper::get($response, "nom"));
+        $model->setScore(ArrayHelper::get($response, "_score"));
+
+        return $model;
+    }
+
+    /**
+     * Deserializes a régions response.
+     *
+     * @param string $rawResponse The raw response.
+     * @return RegionsResponse Returns the régions response.
+     */
+    public static function deserializeRegionsResponse($rawResponse) {
+
+        $model = new RegionsResponse();
+        $model->setRawResponse($rawResponse);
+
+        $response = json_decode($rawResponse, true);
+        if (null === $response) {
+            return $model;
+        }
+
+        foreach ($response as $current) {
+            $model->addRegion(static::deserializeRegion($current));
+        }
+
+        return $model;
+    }
+}
---
+++ tests/Serializer/DecoupageAdministratifResponseDeserializerTest.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Tests\Serializer;
+
+use WBW\Library\GeoAPI\Model\Response\CommunesResponse;
+use WBW\Library\GeoAPI\Model\Response\DepartementsResponse;
+use WBW\Library\GeoAPI\Model\Response\RegionsResponse;
+use WBW\Library\GeoAPI\Serializer\DecoupageAdministratifResponseDeserializer;
+use WBW\Library\GeoAPI\Tests\AbstractTestCase;
+
+/**
+ * Découpage administratif response deserializer test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Tests\Serializer
+ */
+class DecoupageAdministratifResponseDeserializerTest extends AbstractTestCase {
+
+    /**
+     * Tests the deserializeCommunesResponse() method.
+     *
+     * @return void
+     */
+    public function testDeserializeCommunesResponse() {
+
+        // Set a JSON mock.
+        $json = file_get_contents(__DIR__ . "/DecoupageAdministratifResponseDeserializerTest.testDeserializeCommunesResponse.json");
+
+        $res = DecoupageAdministratifResponseDeserializer::deserializeCommunesResponse($json);
+        $this->assertInstanceOf(CommunesResponse::class, $res);
+
+        $this->assertEquals($json, $res->getRawResponse());
+        $this->assertCount(393, $res->getCommunes());
+
+        $this->assertEquals("L'Abergement-Clémenciat", $res->getCommunes()[0]->getNom());
+        $this->assertEquals("01001", $res->getCommunes()[0]->getCode());
+        $this->assertEquals("01", $res->getCommunes()[0]->getCodeDepartement());
+        $this->assertEquals("84", $res->getCommunes()[0]->getCodeRegion());
+        $this->assertEquals(767, $res->getCommunes()[0]->getPopulation());
+        $this->assertNull($res->getCommunes()[0]->getScore());
+    }
+
+    /**
+     * Tests the deserializeDepartementsResponse() method.
+     *
+     * @return void
+     */
+    public function testDeserializeDepartementsResponse() {
+
+        // Set a JSON mock.
+        $json = file_get_contents(__DIR__ . "/DecoupageAdministratifResponseDeserializerTest.testDeserializeDepartementsResponse.json");
+
+        $res = DecoupageAdministratifResponseDeserializer::deserializeDepartementsResponse($json);
+        $this->assertInstanceOf(DepartementsResponse::class, $res);
+
+        $this->assertEquals($json, $res->getRawResponse());
+        $this->assertCount(5, $res->getDepartements());
+
+        $this->assertEquals("Calvados", $res->getDepartements()[0]->getNom());
+        $this->assertEquals("14", $res->getDepartements()[0]->getCode());
+        $this->assertEquals("28", $res->getDepartements()[0]->getCodeRegion());
+        $this->assertNull($res->getDepartements()[0]->getScore());
+    }
+
+    /**
+     * Tests the deserializeRegionsResponse() method.
+     *
+     * @return void
+     */
+    public function testDeserializeRegionsResponse() {
+
+        // Set a JSON mock.
+        $json = file_get_contents(__DIR__ . "/DecoupageAdministratifResponseDeserializerTest.testDeserializeRegionsResponse.json");
+
+        $res = DecoupageAdministratifResponseDeserializer::deserializeRegionsResponse($json);
+        $this->assertInstanceOf(RegionsResponse::class, $res);
+
+        $this->assertEquals($json, $res->getRawResponse());
+        $this->assertCount(18, $res->getRegions());
+
+        $this->assertEquals("Guadeloupe", $res->getRegions()[0]->getNom());
+        $this->assertEquals("01", $res->getRegions()[0]->getCode());
+        $this->assertNull($res->getRegions()[0]->getScore());
+    }
+}
