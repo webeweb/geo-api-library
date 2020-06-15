41378085ce59153496c40b72bdfc237c70cead7d
webeweb <support@webeweb.fr>
2020-06-15 18:28:32 +0200

    Add Région provider

---
+++ src/Provider/RegionProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Provider;
+
+use WBW\Library\Core\Exception\ApiException;
+use WBW\Library\GeoAPI\Model\Request\Region\CommunesRequest;
+use WBW\Library\GeoAPI\Model\Request\Region\DepartementsRequest;
+use WBW\Library\GeoAPI\Model\Request\Region\RegionsRequest;
+use WBW\Library\GeoAPI\Model\Response\CommunesResponse;
+use WBW\Library\GeoAPI\Model\Response\DepartementsResponse;
+use WBW\Library\GeoAPI\Model\Response\RegionsResponse;
+use WBW\Library\GeoAPI\Serializer\DecoupageAdministratifResponseDeserializer;
+
+/**
+ * Région provider.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Provider
+ */
+class RegionProvider extends DecoupageAdministratifProvider {
+
+    /**
+     * Communes.
+     *
+     * @param CommunesRequest $request The communes request.
+     * @return CommunesResponse Returns the communes response.
+     * @throws ApiException Throws an API exception if an error occurs.
+     */
+    public function communes(CommunesRequest $request) {
+
+        $rawResponse = $this->callApi($request, []);
+
+        return DecoupageAdministratifResponseDeserializer::deserializeCommunesResponse($rawResponse);
+    }
+
+    /**
+     * Départements.
+     *
+     * @param DepartementsRequest $request The départements request.
+     * @return DepartementsResponse Returns the départements response.
+     * @throws ApiException Throws an API exception if an error occurs.
+     */
+    public function departements(DepartementsRequest $request) {
+
+        $rawResponse = $this->callApi($request, []);
+
+        return DecoupageAdministratifResponseDeserializer::deserializeDepartementsResponse($rawResponse);
+    }
+
+    /**
+     * Régions.
+     *
+     * @param RegionsRequest $request The régions request.
+     * @return RegionsResponse Returns the régions response.
+     * @throws ApiException Throws an API exception if an error occurs.
+     */
+    public function regions(RegionsRequest $request) {
+
+        $rawResponse = $this->callApi($request, []);
+
+        return DecoupageAdministratifResponseDeserializer::deserializeRegionsResponse($rawResponse);
+    }
+}
---
+++ tests/Provider/RegionProviderTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the geo-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GeoAPI\Tests\Provider;
+
+use Exception;
+use WBW\Library\GeoAPI\Model\Request\Region\DepartementsRequest;
+use WBW\Library\GeoAPI\Model\Request\Region\RegionsRequest;
+use WBW\Library\GeoAPI\Model\Response\DepartementsResponse;
+use WBW\Library\GeoAPI\Model\Response\RegionsResponse;
+use WBW\Library\GeoAPI\Provider\RegionProvider;
+use WBW\Library\GeoAPI\Tests\AbstractTestCase;
+
+/**
+ * Région provider test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GeoAPI\Tests\Provider
+ */
+class RegionProviderTest extends AbstractTestCase {
+
+    /**
+     * Tests the departements() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testDepartements() {
+
+        // Set a Départements request mock.
+        $request = new DepartementsRequest();
+        $request->setCode("11");
+
+        $obj = new RegionProvider($this->logger);
+
+        $res = $obj->departements($request);
+        $this->assertInstanceOf(DepartementsResponse::class, $res);
+    }
+
+    /**
+     * Tests the regions() method.
+     *
+     * @return void
+     * @throws Exception Throws an exception if an error occurs.
+     */
+    public function testRegions() {
+
+        // Set a Régions request mock.
+        $request = new RegionsRequest();
+
+        $obj = new RegionProvider($this->logger);
+
+        $res = $obj->regions($request);
+        $this->assertInstanceOf(RegionsResponse::class, $res);
+    }
+}
